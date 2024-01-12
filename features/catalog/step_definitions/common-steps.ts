16d5da8a98d95cdbcf1b83df6df9bfd9ecbfb4e8
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-01-12 15:22:41 +0100

    Added e-service and descriptor creation common step

--- features/catalog/step_definitions/common-steps.ts
+++ features/catalog/step_definitions/common-steps.ts
@@ -2,6 +2,8 @@ import assert from "assert";
 import { Before, Given, Then, BeforeAll } from "@cucumber/cucumber";
 import { z } from "zod";
 import { generateSessionTokens } from "../../../utils/session-tokens";
+import { EServiceDescriptorState } from "../../../api/models";
+import { dataPreparationService } from "../../../services/data-preparation.service";
 import { assertContextSchema, getRandomInt } from "./../../../utils/commons";
 
 const Party = z.enum(["GSP", "PA1", "PA2", "Privato"]);
@@ -45,5 +47,65 @@ Then(
     });
     assert.equal(this.response.status, statusCode);
     assert.equal(this.response.data.results.length, count);
+  }
+);
+
+Given(
+  "un {string} di {string} ha già creato un e-service con un descrittore in stato {string}",
+  async function (
+    role: Role,
+    party: Party,
+    descriptorState: EServiceDescriptorState
+  ) {
+    assertContextSchema(this);
+
+    if (descriptorState === "ARCHIVED" || descriptorState === "DEPRECATED") {
+      throw new Error(
+        "ARCHIVED and DEPRECATED state not supported by this Given"
+      );
+    }
+
+    const token = this.tokens[party]![role]!;
+
+    // 1. Create e-service
+    const eserviceId = await dataPreparationService.createEService(token);
+
+    // 2. Create DRAFT descriptor
+    const descriptorId = await dataPreparationService.createDraftDescriptor(
+      token,
+      eserviceId
+    );
+
+    this.eserviceId = eserviceId;
+    this.descriptorId = descriptorId;
+
+    if (descriptorState === "DRAFT") {
+      return;
+    }
+
+    // 3. Add interface to descriptor
+    await dataPreparationService.addInterfaceToDescriptor(
+      token,
+      eserviceId,
+      descriptorId
+    );
+
+    // 4. Publish Descriptor
+    await dataPreparationService.publishDescriptor(
+      token,
+      eserviceId,
+      descriptorId
+    );
+
+    if (descriptorState === "PUBLISHED") {
+      return;
+    }
+
+    // 5. Suspend Descriptor
+    await dataPreparationService.suspendDescriptor(
+      token,
+      eserviceId,
+      descriptorId
+    );
   }
 );
