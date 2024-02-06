5bc257be7ae71019569f4abc2375d88361082157
Paolo Manca <154322757+paolomanca-pagopa@users.noreply.github.com>
2024-02-06 11:37:50 +0100

    PIQA-14 Test for clone e-service (#18)

---
+++ features/catalog/step_definitions/e-service-clone.ts
@@ -0,0 +1,47 @@
+import { Given, When } from "@cucumber/cucumber";
+import { z } from "zod";
+import {
+  assertContextSchema,
+  getAuthorizationHeader,
+} from "../../../utils/commons";
+import { apiClient } from "../../../api";
+import { EServiceDescriptorState } from "../../../api/models";
+import { dataPreparationService } from "./../../../services/data-preparation.service";
+import { Party, Role } from "./common-steps";
+
+Given(
+  "un {string} di {string} ha già creato una versione in {string} per quell'eservice",
+  async function (
+    role: Role,
+    party: Party,
+    descriptorState: EServiceDescriptorState
+  ) {
+    assertContextSchema(this, {
+      eserviceId: z.string(),
+    });
+
+    const token = this.tokens[party]![role]!;
+
+    const { descriptorId } =
+      await dataPreparationService.createDescriptorWithGivenState({
+        token,
+        eserviceId: this.eserviceId,
+        descriptorState,
+      });
+
+    this.descriptorId = descriptorId;
+  }
+);
+
+When("l'utente clona quell'e-service", async function () {
+  assertContextSchema(this, {
+    token: z.string(),
+    eserviceId: z.string(),
+    descriptorId: z.string(),
+  });
+  this.response = await apiClient.eservices.cloneEServiceByDescriptor(
+    this.eserviceId,
+    this.descriptorId,
+    getAuthorizationHeader(this.token)
+  );
+});
