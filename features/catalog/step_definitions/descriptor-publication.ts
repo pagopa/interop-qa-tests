2b801b4331755601ecb48c10c7cf073750dddfa7
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-02-09 15:43:14 +0100

    PIQA-23 Test for risk analysis read (#28)

--- features/catalog/step_definitions/descriptor-publication.ts
+++ features/catalog/step_definitions/descriptor-publication.ts
@@ -44,11 +44,12 @@ Given(
 
     // If descriptorState is not DRAFT we have to add a completed risk analysis in order to correctly publish the descriptor
     if (mode === "RECEIVE" && descriptorState !== "DRAFT") {
-      await dataPreparationService.addRiskAnalysisToEService(
-        token,
-        this.eserviceId,
-        getRiskAnalysis({ completed: true, party: this.party })
-      );
+      this.riskAnalysisId =
+        await dataPreparationService.addRiskAnalysisToEService(
+          token,
+          this.eserviceId,
+          getRiskAnalysis({ completed: true, party: this.party })
+        );
     }
 
     const { descriptorId } =
