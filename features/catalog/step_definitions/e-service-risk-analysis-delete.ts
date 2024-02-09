0f5d55111bc7c74a337708eaf081d48bc7d00e9e
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-02-09 15:47:11 +0100

    PIQA-25 Test for delete eservice risk analysis (#30)

---
+++ features/catalog/step_definitions/e-service-risk-analysis-delete.ts
@@ -0,0 +1,23 @@
+import { When } from "@cucumber/cucumber";
+import { z } from "zod";
+import { apiClient } from "../../../api";
+import {
+  assertContextSchema,
+  getAuthorizationHeader,
+} from "../../../utils/commons";
+
+When(
+  "l'utente cancella quell'analisi del rischio di quell'e-service",
+  async function () {
+    assertContextSchema(this, {
+      token: z.string(),
+      eserviceId: z.string(),
+      riskAnalysisId: z.string(),
+    });
+    this.response = await apiClient.eservices.deleteEServiceRiskAnalysis(
+      this.eserviceId,
+      this.riskAnalysisId,
+      getAuthorizationHeader(this.token)
+    );
+  }
+);
