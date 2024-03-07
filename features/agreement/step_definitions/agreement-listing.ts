6a16b81dbb2b3f74d3f8ea854b84ac325009efda
AsterITA <fabrizio.musella@gmail.com>
2024-03-07 16:40:27 +0100

    CI test undefined

--- features/agreement/step_definitions/agreement-listing.ts
+++ features/agreement/step_definitions/agreement-listing.ts
@@ -13,7 +13,7 @@ import { TenantType, SessionTokens, Role } from "../../common-steps";
 import { AgreementState } from "../../../api/models";
 
 Given(
-  "un {string} di {string} ha già creato e pubblicato {int} e-service(s)",
+  "X {string} di {string} ha già creato e pubblicato {int} e-service(s)",
   async function (role: Role, tenantType: TenantType, totalEservices: number) {
     assertContextSchema(this);
     const token = getToken(this.tokens, tenantType, role);
@@ -40,34 +40,7 @@ Given(
 );
 
 Given(
-  "un {string} di {string} ha già creato e pubblicato {int} e-service(s)",
-  async function (role: Role, tenantType: TenantType, totalEservices: number) {
-    assertContextSchema(this);
-    const token = getToken(this.tokens, tenantType, role);
-
-    const arr = new Array(totalEservices).fill(0);
-    const createEServiceWithPublishedDescriptor = async (i: number) => {
-      const eserviceId = await dataPreparationService.createEService(token, {
-        name: `eservice-${i}-${this.TEST_SEED}`,
-      });
-      const { descriptorId } =
-        await dataPreparationService.createDescriptorWithGivenState({
-          token,
-          eserviceId,
-          descriptorState: "PUBLISHED",
-        });
-
-      return [eserviceId, descriptorId];
-    };
-
-    this.publishedEservicesIds = await Promise.all(
-      arr.map((_, i) => createEServiceWithPublishedDescriptor(i))
-    );
-  }
-);
-
-Given(
-  "{string} ha un X attivo per ciascun e-service di {string}",
+  "{string} ha un agreement attivo per ciascun e-service di {string}",
   async function (consumer: TenantType, _producer: string) {
     assertContextSchema(this, {
       token: z.string(),
