3afd9f0431b4ecf8097268520c8d21d30ae27e1c
borgesis95 <salvatore.borgesi@pagopa.it>
2024-03-08 15:25:09 +0100

    updated agrement-producer-listing feature and step

--- features/agreement/step_definitions/agreement-producer-listing.ts
+++ features/agreement/step_definitions/agreement-producer-listing.ts
@@ -68,8 +68,13 @@ When(
     assertContextSchema(this, {
       token: z.string(),
     });
-    this.response = await apiClient.agreements.getAgreementConsumers(
+    this.responseOffsetOne = await apiClient.agreements.getAgreementConsumers(
       { limit: 50, offset },
+      getAuthorizationHeader(this.token)
+    );
+
+    this.responseOffsetTwo = await apiClient.agreements.getAgreementConsumers(
+      { limit: 50, offset: offset + 1 },
       getAuthorizationHeader(this.token)
     );
   }
@@ -102,5 +107,41 @@ Then(
 
     assert.equal(this.response.status, statusCode);
     assert.equal(this.response.data.results.length, consumers);
+  }
+);
+
+Then(
+  "si ottiene status code 200 con la corretta verifica dell'offset",
+  async function () {
+    assertContextSchema(this, {
+      responseOffsetOne: z.object({
+        status: z.number(),
+        data: z.object({
+          results: z.array(
+            z.object({
+              id: z.string(),
+            })
+          ),
+        }),
+      }),
+      responseOffsetTwo: z.object({
+        status: z.number(),
+        data: z.object({
+          results: z.array(
+            z.object({
+              id: z.string(),
+            })
+          ),
+        }),
+      }),
+    });
+
+    assert.equal(this.responseOffsetOne.status, 200);
+    assert.equal(this.responseOffsetTwo.status, 200);
+
+    assert.equal(
+      this.responseOffsetOne.data.results[1].id,
+      this.responseOffsetTwo.data.results[0].id
+    );
   }
 );
