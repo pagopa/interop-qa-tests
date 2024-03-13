fc7d031209394b3aeb0a9ed9ad58312f018fdd6d
Paolo Manca <paolo.manca@pagopa.it>
2024-03-13 15:03:41 +0100

    new profile management: small composeabile profiles, adding npm script to exec test per single module

--- cucumber.mjs
+++ cucumber.mjs
@@ -1,20 +1,45 @@
-const common = {
-  paths: ["features/**/*.feature"],
-  require: ["./features/**/step_definitions/**/*.ts"],
+export const parallel = {
   parallel: Number(process.env.CUCUMBER_OPTS_PARALLEL) || 1,
 };
-const format = {
+export const format = {
   format: ["progress-bar"],
   formatOptions: { snippetInterface: "synchronous" },
 };
-
-export default {
-  ...common,
+export const base = {
+  ...parallel,
   ...format,
+};
+
+export const validate = {
+  dryRun: true,
+};
+
+export const node = {
   requireModule: ["ts-node/register"],
 };
 
-export const bun = {
-  ...common,
-  ...format,
+export const all = {
+  paths: ["features/**/*.feature"],
+  require: ["./features/**/step_definitions/**/*.ts"],
 };
+
+export const catalog = {
+  paths: ["features/catalog/*.feature"],
+  require: ["./features/**/step_definitions/**/*.ts"],
+};
+
+export const attribute = {
+  paths: ["features/attribute/*.feature"],
+  require: ["./features/**/step_definitions/**/*.ts"],
+};
+
+export const agreement = {
+  paths: ["features/agreement/*.feature"],
+  require: ["./features/**/step_definitions/**/*.ts"],
+};
+
+export const onlyReady = {
+  tags: "(not @wait_for_fix) and (not @resource_intensive)",
+};
+
+export default {};
