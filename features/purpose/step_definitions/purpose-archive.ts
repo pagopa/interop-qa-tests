f09645045583ffafbca31433209c1219e6048c54
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-03-22 16:49:06 +0100

    Minor change and add template

--- features/purpose/step_definitions/purpose-archive.ts
+++ features/purpose/step_definitions/purpose-archive.ts
@@ -1,3 +1,8 @@
-import { When } from "@cucumber/cucumber";
+import { Given, When } from "@cucumber/cucumber";
 
 When("l'utente archivia quella finalità", async function () {});
+
+Given(
+  "l'utente ha già richiesto l'aggiornamento della stima di carico superando i limiti di quell'e-service",
+  async function () {}
+);
