ed7e9885f63a7b84c77669f9c3d82a570a6a6207
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-03-22 11:50:09 +0100

    changed Given

--- features/purpose/step_definitions/purpose-creation-mode-receive.ts
+++ features/purpose/step_definitions/purpose-creation-mode-receive.ts
@@ -78,18 +78,18 @@ When(
 );
 
 Given(
-  "l'utente ha già creato una finalità in stato \"DRAFT\" per quell'eservice associando quell'analisi del rischio creata dall'erogatore",
-  async function () {
+  "un {string} di {string} ha già creato una finalità in stato \"DRAFT\" per quell'eservice associando quell'analisi del rischio creata dall'erogatore",
+  async function (role: Role, tenantType: TenantType) {
     assertContextSchema(this, {
-      token: z.string(),
       eserviceId: z.string(),
       riskAnalysisId: z.string(),
     });
+    const token = getToken(this.tokens, tenantType, role);
 
-    const consumerId = getOrganizationId(this.tenantType);
+    const consumerId = getOrganizationId(tenantType);
 
     await dataPreparationService.createPurposeForReceiveEservice(
-      this.token,
+      token,
       this.TEST_SEED,
       {
         eserviceId: this.eserviceId,
