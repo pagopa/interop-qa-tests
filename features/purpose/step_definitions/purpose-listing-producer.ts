54e2f1a0c497f80f19f7dd252ca578debafc1775
AsterITA <fabrizio.musella@gmail.com>
2024-04-04 14:47:07 +0200

    fix steps

--- features/purpose/step_definitions/purpose-listing-producer.ts
+++ features/purpose/step_definitions/purpose-listing-producer.ts
@@ -165,7 +165,7 @@ Given(
       eserviceId: z.string(),
     });
 
-    const token = getToken(this.tokens, tenantType, "admin");
+    const token = await getToken(tenantType);
     const consumerId = getOrganizationId(tenantType);
 
     const { riskAnalysisForm } = getRiskAnalysis({
