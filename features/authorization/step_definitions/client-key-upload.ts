fc7546cc246a7cb43407de1ae7641b27535be32e
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-06-13 10:23:22 +0200

    Added templates

---
+++ features/authorization/step_definitions/client-key-upload.ts
@@ -0,0 +1,36 @@
+import { When } from "@cucumber/cucumber";
+
+When(
+  "l'utente richiede il caricamento di una chiave pubblica di tipo {string}",
+  (_keyType: string) => {
+    console.log("TODO");
+  }
+);
+
+When(
+  "l'utente richiede il caricamento di una chiave pubblica di tipo {string} di lunghezza {int}",
+  (_keyType: string, _keyLength: number) => {
+    console.log("TODO");
+  }
+);
+
+When(
+  "l'utente richiede il caricamento di una chiave pubblica di tipo {string} di lunghezza {int}",
+  (_keyType: string, _keyLength: number) => {
+    console.log("TODO");
+  }
+);
+
+When(
+  "l'utente richiede il caricamento di una chiave pubblica di tipo {string} di lunghezza {int} senza i delimitatori di inizio e fine",
+  (_keyType: string, _keyLength: number) => {
+    console.log("TODO");
+  }
+);
+
+When(
+  "l'utente richiede il caricamento di una chiave pubblica di tipo {string} di lunghezza {int} con lo stesso kid",
+  (_keyType: string, _keyLength: number) => {
+    console.log("TODO");
+  }
+);
