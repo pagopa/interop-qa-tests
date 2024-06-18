c1e2d5806048ff3d65ecc2225a8f03d4cd4d564a
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-06-18 17:46:22 +0200

    PIQA-95 Client users listing (#114)

---
+++ features/authorization/step_definitions/client-users-listing.ts
@@ -0,0 +1,36 @@
+import assert from "assert";
+import { Then, When } from "@cucumber/cucumber";
+import { z } from "zod";
+import { apiClient } from "../../../api";
+import {
+  assertContextSchema,
+  getAuthorizationHeader,
+} from "../../../utils/commons";
+
+When(
+  "l'utente richiede una operazione di listing dei membri di quel client",
+  async function () {
+    assertContextSchema(this, {
+      token: z.string(),
+      clientId: z.string(),
+    });
+    this.response = await apiClient.clients.getClientUsers(
+      this.clientId,
+      getAuthorizationHeader(this.token)
+    );
+  }
+);
+
+Then(
+  "si ottiene status code 200 e la lista di {int} utenti",
+  async function (count: number) {
+    assertContextSchema(this, {
+      response: z.object({
+        status: z.number(),
+        data: z.array(z.unknown()),
+      }),
+    });
+    assert.equal(this.response.status, 200);
+    assert.equal(this.response.data.length, count);
+  }
+);
