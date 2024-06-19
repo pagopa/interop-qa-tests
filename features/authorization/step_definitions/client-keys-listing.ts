c011545ddef16f32dd79be82c9ba28808a45e5b3
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-06-19 11:43:28 +0200

    PIQA-97 Client keys listing (#119)

---
+++ features/authorization/step_definitions/client-keys-listing.ts
@@ -0,0 +1,62 @@
+import assert from "assert";
+import { Then, When } from "@cucumber/cucumber";
+import { z } from "zod";
+import {
+  Role,
+  TenantType,
+  assertContextSchema,
+  getAuthorizationHeader,
+  getUserId,
+} from "../../../utils/commons";
+import { apiClient } from "../../../api";
+
+When(
+  "l'utente richiede una operazione di listing delle chiavi di quel client",
+  async function () {
+    assertContextSchema(this, {
+      token: z.string(),
+      clientId: z.string(),
+    });
+
+    this.response = await apiClient.clients.getClientKeys(
+      { clientId: this.clientId },
+      getAuthorizationHeader(this.token)
+    );
+  }
+);
+
+Then(
+  "si ottiene status code {int} e la lista di {int} chiavi",
+  async function (statusCode: number, numKeys: number) {
+    assertContextSchema(this, {
+      response: z.object({
+        status: z.number(),
+        data: z.object({
+          keys: z.array(z.unknown()),
+        }),
+      }),
+    });
+
+    assert.equal(this.response.status, statusCode);
+    assert.equal(this.response.data.keys.length, numKeys);
+  }
+);
+
+When(
+  "l'utente richiede una operazione di listing delle chiavi di quel client create dall'utente {string}",
+  async function (role: Role) {
+    assertContextSchema(this, {
+      token: z.string(),
+      clientId: z.string(),
+      tenantType: z.string(),
+    });
+
+    this.response = await apiClient.clients.getClientKeys(
+      {
+        userIds: [getUserId(this.tenantType as TenantType, role)],
+        clientId: this.clientId,
+      },
+      getAuthorizationHeader(this.token)
+    );
+  }
+);
