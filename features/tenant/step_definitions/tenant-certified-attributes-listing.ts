4420e80ad0ee08f29ccc91d60d26e601d216c9e9
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-06-20 10:49:03 +0200

    Added implementation

--- features/tenant/step_definitions/tenant-certified-attributes-listing.ts
+++ features/tenant/step_definitions/tenant-certified-attributes-listing.ts
@@ -1,9 +1,60 @@
+import assert from "assert";
 import { When } from "@cucumber/cucumber";
-import { TenantType } from "../../../utils/commons";
+import { z } from "zod";
+import {
+  TenantType,
+  assertContextSchema,
+  getAuthorizationHeader,
+  getOrganizationId,
+} from "../../../utils/commons";
+import { apiClient } from "../../../api";
 
 When(
   "l'utente richiede una operazione di listing degli attributi certificati posseduti da {string}",
-  function (_tenantType: TenantType) {
-    return "pending";
+  async function (tenantType: TenantType) {
+    assertContextSchema(this, {
+      token: z.string(),
+    });
+
+    const tenantId = getOrganizationId(tenantType);
+
+    this.response = await apiClient.tenants.getCertifiedAttributes(
+      tenantId,
+      getAuthorizationHeader(this.token)
+    );
+  }
+);
+
+When(
+  "si ottiene status code 200 e la lista degli attributi certificati contenente l'attributo assegnato e l'attributo IPA comune",
+  async function () {
+    assertContextSchema(this, {
+      attributeId: z.string(),
+      response: z.object({
+        status: z.number(),
+        data: z.object({
+          attributes: z.array(
+            z.object({
+              id: z.string(),
+              name: z.string(),
+            })
+          ),
+        }),
+      }),
+    });
+
+    assert.equal(this.response.status, 200);
+    assert.ok(
+      this.response.data.attributes.some(
+        (attr) => attr.id === this.attributeId
+      ),
+      "L'attributo assegnato non è presente nella lista degli attributi certificati"
+    );
+    assert.ok(
+      this.response.data.attributes.some(
+        (attr) => attr.name === "Comuni e loro Consorzi e Associazioni"
+      ),
+      "L'attributo IPA comune non è presente nella lista degli attributi certificati"
+    );
   }
 );
