41ef9c6112cea3e5e1fcbd173688542d5dc90e5f
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-06-25 11:35:43 +0200

    Revert change

--- features/attribute/step_definitions/attribute-common-steps.ts
+++ features/attribute/step_definitions/attribute-common-steps.ts
@@ -7,6 +7,32 @@ import { AttributeKind } from "../../../api/models";
 import { dataPreparationService } from "../../../services/data-preparation.service";
 
 Given(
+  "{string} ha già creato {int} attribut(i)(o) {string}",
+  async function (
+    tenantType: TenantType,
+    count: number,
+    attributeKind: AttributeKind
+  ) {
+    assertContextSchema(this);
+    const token = await getToken(tenantType);
+
+    const promises = [];
+
+    for (let i = 0; i < count; i++) {
+      promises.push(
+        dataPreparationService.createAttribute(
+          token,
+          attributeKind,
+          `attribute-${i}-${this.TEST_SEED}-${attributeKind}`
+        )
+      );
+    }
+
+    this.attributeId = (await Promise.all(promises))[0];
+  }
+);
+
+Given(
   "{string} ha già creato un attributo {string} con nome che contiene {string}",
   async function (
     tenantType: TenantType,
