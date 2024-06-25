7dc089030eed0b96011f9d989b178986867a2cc7
Roberto Taglioni <roberto.taglioni@pagopa.it>
2024-06-25 11:15:05 +0200

    Add implementation

--- features/tenant/step_definitions/tenant-assign-certified-attribute.ts
+++ features/tenant/step_definitions/tenant-assign-certified-attribute.ts
@@ -1,8 +1,24 @@
 import { When } from "@cucumber/cucumber";
+import { z } from "zod";
+import {
+  TenantType,
+  assertContextSchema,
+  getAuthorizationHeader,
+  getOrganizationId,
+} from "../../../utils/commons";
+import { apiClient } from "../../../api";
 
 When(
   "l'utente assegna a {string} l'attributo certificato precedentemente creato",
-  async function () {
-    return "pending";
+  async function (tenantType: TenantType) {
+    assertContextSchema(this, { token: z.string(), attributeId: z.string() });
+    const tenantId = getOrganizationId(tenantType);
+    this.response = await apiClient.tenants.addCertifiedAttribute(
+      tenantId,
+      {
+        id: this.attributeId,
+      },
+      getAuthorizationHeader(this.token)
+    );
   }
 );
