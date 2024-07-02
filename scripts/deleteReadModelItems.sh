3f2062eef960a2a1ce803a5cbc6d0e2a8a48b932
Michele De Simone <michele.desimone@pagopa.it>
2024-07-02 11:13:55 +0200

    fix: mongosh connection

--- scripts/deleteReadModelItems.sh
+++ scripts/deleteReadModelItems.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 set -euo pipefail
-# mongodb://"${encodedAdminUser}":"${encodedAdminPassword}"@"$READ_MODEL_DB_HOST":"$READ_MODEL_DB_PORT"/admin
-mongosh mongodb://"$READ_MODEL_USERNAME":"$READ_MODEL_PASSWORD"@"$READ_MODEL_HOST":"$READ_MODEL_PORT/${READ_MODEL_DB_NAME}" \
+
+mongosh -u $READ_MODEL_USERNAME -p $READ_MODEL_PASSWORD --host $READ_MODEL_HOST --port $READ_MODEL_PORT $READ_MODEL_DB_NAME \
   --eval "var collections = db.getCollectionNames(); 
           for (let coll of collections) {
             db.getCollection(coll).deleteMany({});
-          }
-          exit;" 
+          }" 
