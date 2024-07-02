f71e4a93d4c6776e32c33612a9095d93be8232bb
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-07-02 10:04:51 +0200

    Fix lint problems

--- scripts/kafka/utils/kafkaTopic.js
+++ scripts/kafka/utils/kafkaTopic.js
@@ -1,99 +1,106 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import * as kafkaConfigUtility from "./kafkaConfigs.js";
 
 export class KafkaTopic {
+  #topicConfig = {
+    topic: null,
+    configEntries: kafkaConfigUtility.getTopicDefaultConfiguration(),
+  };
 
-    #topicConfig = {
-        topic: null,
-        configEntries: kafkaConfigUtility.getTopicDefaultConfiguration()
-    };
+  getTopicName() {
+    return structuredClone(this.#topicConfig.topic);
+  }
 
-    constructor() {}
+  getTopicOptions() {
+    const keys = Object.keys(this.#topicConfig);
+    const opts = {};
 
-    getTopicName() {
-        return structuredClone(this.#topicConfig['topic']);
+    for (const k of keys) {
+      if (k !== "topic" && k !== "configEntries") {
+        opts[k] = this.#topicConfig[k];
+      }
     }
 
-    getTopicOptions() {
-        const keys = Object.keys(this.#topicConfig);
-        const opts = {};
+    return structuredClone(opts);
+  }
 
-        for (let k of keys) {
-            if (k !== 'topic' && k !== 'configEntries') {
-                opts[k] = this.#topicConfig[k];
-            }
-        }
-
-        return structuredClone(opts);
+  getTopicOption(optionName) {
+    let val = null;
+    if (this.#topicConfig[optionName]) {
+      val = structuredClone(this.#topicConfig[optionName]);
     }
 
-    getTopicOption(optionName) {
-        let val = null;
-        if (this.#topicConfig[optionName]) 
-            val = structuredClone(this.#topicConfig[optionName]);
+    return val;
+  }
 
-        return val;
+  getTopicConfigurations() {
+    return structuredClone(this.#topicConfig.configEntries);
+  }
+
+  getTopicConfiguration(configName) {
+    return structuredClone(
+      this.#topicConfig.configEntries.filter((ce) => ce.name === configName)
+    );
+  }
+
+  getTopic() {
+    return structuredClone(this.#topicConfig);
+  }
+
+  setTopicConfig(configName, configValue) {
+    if (kafkaConfigUtility.isTopicNameProperty(configName)) {
+      this.#topicConfig.topic = configValue;
+    } else if (kafkaConfigUtility.isTopicRequiredOption(configName)) {
+      const optionToOverride =
+        kafkaConfigUtility.getTopicRequiredOptionKey(configName);
+      if (optionToOverride) {
+        this.#topicConfig[optionToOverride] = configValue;
+      }
+    } else {
+      let overrideIndex = -1;
+      if (kafkaConfigUtility.isTopicDefaultConfig(configName)) {
+        overrideIndex = this.#topicConfig.configEntries.findIndex(
+          (ce) => ce.name === configName
+        );
+      }
+
+      if (overrideIndex !== -1) {
+        this.#topicConfig.configEntries[overrideIndex].value = configValue;
+      } else {
+        this.#topicConfig.configEntries.push({
+          name: configName,
+          value: configValue,
+        });
+      }
+    }
+  }
+
+  isValidTopicConfiguration() {
+    const topicConfig = this.#topicConfig;
+
+    if (!topicConfig.topic) {
+      return {
+        valid: false,
+        error: `Topic name cannot be null`,
+      };
     }
 
-    getTopicConfigurations() {
-        return structuredClone(this.#topicConfig.configEntries);
-    }
-    
-    getTopicConfiguration(configName) {
-        return structuredClone(this.#topicConfig.configEntries.filter(ce => ce.name === configName));
-    }
-
-    getTopic() {
-        return structuredClone(this.#topicConfig);
-    }
-
-    setTopicConfig(configName, configValue) {
-        if (kafkaConfigUtility.isTopicNameProperty(configName)) {
-            this.#topicConfig['topic'] = configValue;
-        } else if (kafkaConfigUtility.isTopicRequiredOption(configName)) {
-            let optionToOverride = kafkaConfigUtility.getTopicRequiredOptionKey(configName);
-            if (optionToOverride) {
-                this.#topicConfig[optionToOverride] = configValue;
-            }
-        } else {
-            let overrideIndex = -1;
-            if (kafkaConfigUtility.isTopicDefaultConfig(configName)) {
-                overrideIndex = this.#topicConfig["configEntries"].findIndex(ce => ce.name === configName);
-            }
-
-            if (overrideIndex != -1) {
-                this.#topicConfig['configEntries'][overrideIndex].value = configValue;
-            } else {
-                this.#topicConfig['configEntries'].push({
-                    name: configName,
-                    value: configValue
-                });
-            }
-        }
-    }
-
-    isValidTopicConfiguration() {
-        let topicConfig = this.#topicConfig;
-
-        if (!topicConfig['topic']) {
-            return {
-                valid: false,
-                error: `Topic name cannot be null`
-            };
-        }
-
-        const requiredOptions = kafkaConfigUtility.getTopicRequiredOptions();
-        for ( let requiredOption of requiredOptions) {
-            if (typeof topicConfig[requiredOption.targetLabel]  == "undefined" || topicConfig[requiredOption.targetLabel] == null) {
-                return {
-                    valid: false,
-                    error: `Topic configuration is missing required option ${requiredOption.expectedLabel}`
-                }
-            }
-        }
-        
+    const requiredOptions = kafkaConfigUtility.getTopicRequiredOptions();
+    for (const requiredOption of requiredOptions) {
+      if (
+        typeof topicConfig[requiredOption.targetLabel] == "undefined" ||
+        topicConfig[requiredOption.targetLabel] == null
+      ) {
         return {
-            valid: true,
-            error: null
-        };   
+          valid: false,
+          error: `Topic configuration is missing required option ${requiredOption.expectedLabel}`,
+        };
+      }
     }
+
+    return {
+      valid: true,
+      error: null,
+    };
+  }
 }
--- scripts/kafka/utils/propertiesParser.js
+++ scripts/kafka/utils/propertiesParser.js
@@ -1,40 +1,41 @@
-
 import { isTopicOption } from "./kafkaConfigs.js";
 
 export function normalizeQuotes(stringToSanitize) {
-    let sanitized = stringToSanitize.replaceAll("\"", '');
-    sanitized = sanitized.replaceAll("'", '');
+  let sanitized = stringToSanitize.replaceAll('"', "");
+  sanitized = sanitized.replaceAll("'", "");
 
-    return sanitized;
+  return sanitized;
 }
 
-export function sanitizePropsLine (propKey, propValue) {
-    let sanitizedKey = normalizeQuotes(propKey);
-    let sanitizedValue = normalizeQuotes(propValue);
-    
-    if (isTopicOption(sanitizedKey)) {
-        sanitizedValue = JSON.parse(sanitizedValue);
-    }
+export function sanitizePropsLine(propKey, propValue) {
+  const sanitizedKey = normalizeQuotes(propKey);
+  let sanitizedValue = normalizeQuotes(propValue);
 
-    return {
-        key: sanitizedKey,
-        value: sanitizedValue
-    }
-}
-    
-export function parsePropsLine (propsEntry) {
-    let currentLine = propsEntry.split("=");
-    if (currentLine.length != 2) {
-        throw new Error(`Topic configuration file ${topicConfigurationFile} is not properly formatted.`);
-    }
+  if (isTopicOption(sanitizedKey)) {
+    sanitizedValue = JSON.parse(sanitizedValue);
+  }
 
-    return sanitizePropsLine(currentLine[0], currentLine[1]);
+  return {
+    key: sanitizedKey,
+    value: sanitizedValue,
+  };
 }
 
-export function isCommentLine (line) {
-    return line && (line.startsWith("#") || line.startsWith("/"));
+export function parsePropsLine(propsEntry) {
+  const currentLine = propsEntry.split("=");
+  if (currentLine.length !== 2) {
+    throw new Error(
+      `Topic configuration file ${propsEntry} is not properly formatted.`
+    );
+  }
+
+  return sanitizePropsLine(currentLine[0], currentLine[1]);
 }
 
-export function shouldIgnoreLine (line) {
-    return !line || isCommentLine(line);
+export function isCommentLine(line) {
+  return line && (line.startsWith("#") || line.startsWith("/"));
+}
+
+export function shouldIgnoreLine(line) {
+  return !line || isCommentLine(line);
 }
