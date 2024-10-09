bda12c30e61651f1558a203f707f1191abe90952
Roberto Gregnanin <roberto.gregnanin@pagopa.it>
2024-10-09 10:22:59 +0200

    fix lint

--- features/voucher/step_definitions/voucher-generation.ts
+++ features/voucher/step_definitions/voucher-generation.ts
@@ -40,22 +40,19 @@ When(
     });
 
     const { publicKey, privateKey, clientId, purposeId } = this;
-    const kid = calculateKidFromPublicKey(publicKey);
 
     const clientAssertion = createClientAssertion({
-      kid,
       clientId,
       purposeId,
+      publicKey,
       privateKey,
       includeDigest: true,
     });
 
-    const result = await requestVoucher({
+    this.response = await requestVoucher({
       clientId,
       clientAssertion,
     });
-
-    this.result = result;
   }
 );
 
@@ -71,12 +68,11 @@ When(
 
     const { newClientPublicKey, newClientPrivateKey, clientId, purposeId } =
       this;
-    const kid = calculateKidFromPublicKey(newClientPublicKey);
 
     const clientAssertion = createClientAssertion({
-      kid,
       clientId,
       purposeId,
+      publicKey: newClientPublicKey,
       privateKey: newClientPrivateKey,
     });
 
